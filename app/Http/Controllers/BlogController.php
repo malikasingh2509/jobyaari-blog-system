af41b07e28451b616c80ddb7f4420587c23d80e4
Malika Singh <v9d@MacBook-Air.local>
2026-05-08 15:41:28 +0530

    fix image uploads finally

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -47,10 +47,17 @@ class BlogController extends Controller
 
         if ($request->hasFile('image')) {
 
-            $imageName = time().'.'.$request->image->extension();
+            $destinationPath = public_path('images');
+
+            // Create folder if not exists
+            if (!file_exists($destinationPath)) {
+                mkdir($destinationPath, 0777, true);
+            }
+
+            $imageName = time() . '.' . $request->image->extension();
 
             $request->image->move(
-                public_path('images'),
+                $destinationPath,
                 $imageName
             );
         }
@@ -82,10 +89,17 @@ class BlogController extends Controller
 
         if ($request->hasFile('image')) {
 
-            $imageName = time().'.'.$request->image->extension();
+            $destinationPath = public_path('images');
+
+            // Create folder if not exists
+            if (!file_exists($destinationPath)) {
+                mkdir($destinationPath, 0777, true);
+            }
+
+            $imageName = time() . '.' . $request->image->extension();
 
             $request->image->move(
-                public_path('images'),
+                $destinationPath,
                 $imageName
             );
         }
@@ -112,26 +126,36 @@ class BlogController extends Controller
     {
         $blogs = Blog::query();
 
-        if($request->search)
-        {
-            $blogs->where('title', 'like', '%'.$request->search.'%');
+        // SEARCH
+        if ($request->search) {
+            $blogs->where(
+                'title',
+                'like',
+                '%' . $request->search . '%'
+            );
         }
 
-        if($request->category)
-        {
-            $blogs->where('category', $request->category);
+        // CATEGORY
+        if ($request->category) {
+            $blogs->where(
+                'category',
+                $request->category
+            );
         }
 
-        if($request->date)
-        {
-            $blogs->whereDate('created_at', $request->date);
+        // DATE
+        if ($request->date) {
+            $blogs->whereDate(
+                'created_at',
+                $request->date
+            );
         }
 
         $blogs = $blogs->latest()->get();
 
         $output = '';
 
-        foreach($blogs as $blog)
+        foreach ($blogs as $blog)
         {
             $output .= '
 
@@ -139,11 +163,11 @@ class BlogController extends Controller
 
                 <div class="card h-100 shadow-sm">';
 
-            if($blog->image)
+            if ($blog->image)
             {
                 $output .= '
 
-                    <img src="/images/'.$blog->image.'"
+                    <img src="/images/' . $blog->image . '"
                          class="card-img-top"
                          height="200"
                          style="object-fit:cover;">';
@@ -154,55 +178,55 @@ class BlogController extends Controller
                 <div class="card-body d-flex flex-column">
 
                     <h5 class="card-title">
-                        '.$blog->title.'
+                        ' . $blog->title . '
                     </h5>
 
                     <p class="card-text">
-                        '.$blog->short_description.'
+                        ' . $blog->short_description . '
                     </p>
 
                     <p>
-                    <strong>Category:</strong>
-                      '.$blog->category.'
+                        <strong>Category:</strong>
+                        ' . $blog->category . '
                     </p>
 
                     <p class="text-muted small">
-                      Posted on:
-                      '.$blog->created_at->format('d M Y, h:i A').'
+                        Posted on:
+                        ' . $blog->created_at->format('d M Y, h:i A') . '
                     </p>
 
                     <div class="mt-auto">
 
-                    <a href="/blogs/'.$blog->id.'"
-                      class="btn btn-primary btn-sm">
-                       View
-                    </a>
+                        <a href="/blogs/' . $blog->id . '"
+                           class="btn btn-primary btn-sm">
+                           View
+                        </a>
 
-                    <a href="/blogs/'.$blog->id.'/edit"
-                         class="btn btn-warning btn-sm">
-                        Edit
-                    </a>
+                        <a href="/blogs/' . $blog->id . '/edit"
+                           class="btn btn-warning btn-sm">
+                           Edit
+                        </a>
 
-                    <form action="/blogs/'.$blog->id.'"
-                        method="POST"
-                        style="display:inline;">
+                        <form action="/blogs/' . $blog->id . '"
+                              method="POST"
+                              style="display:inline;">
 
-                        '.csrf_field().'
-                        '.method_field('DELETE').'
+                            ' . csrf_field() . '
+                            ' . method_field('DELETE') . '
 
-                        <button class="btn btn-danger btn-sm">
-                          Delete
-                        </button>
+                            <button class="btn btn-danger btn-sm">
+                                Delete
+                            </button>
 
-                    </form>
+                        </form>
 
-                </div>
-
-                </div>
+                    </div>
 
                 </div>
 
-            </div>';
+            </div>
+
+        </div>';
         }
 
         return $output;
