f343a4d065795f069607c82d306989cd40e84c67
Malika Singh <v9d@MacBook-Air.local>
2026-05-08 11:50:49 +0530

    fix admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,25 +2,15 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
-   public function run(): void
-{
-    \App\Models\User::updateOrCreate(
-        ['email' => 'admin@jobyaari.com'],
-        [
-            'name' => 'Admin',
-            'password' => \Illuminate\Support\Facades\Hash::make('admin123'),
-        ]
-    );
-}
+    public function run(): void
+    {
+        $this->call(AdminSeeder::class);
+    }
 }
